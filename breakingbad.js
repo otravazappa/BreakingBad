40ce640e70926e6b3b41ff1971f67e86b45b679c
otravazappa <136837897+otravazappa@users.noreply.github.com>
2023-11-02 15:10:28 -0300

    .

--- breakingbad.js
+++ breakingbad.js
@@ -1,39 +1,40 @@
-
-const gallery = document.getElementById('gallery');
-const images = gallery.querySelectorAll('.image-container');
-const totalImages = images.length;
-let currentImageIndex = 0;
-
-function showImage(index) {
-    if (index >= 0 && index < totalImages) {
-        images[currentImageIndex].classList.remove('active');
-        images[index].classList.add('active');
+document.addEventListener("DOMContentLoaded", function () {
+    const gallery = document.getElementById("gallery");
+    const images = gallery.querySelectorAll(".image-container");
+    const totalImages = images.length;
+    let currentImageIndex = 0;
+  
+    function showImage(index) {
+      if (index >= 0 && index < totalImages) {
+        images[currentImageIndex].classList.remove("active");
+        images[index].classList.add("active");
         currentImageIndex = index;
+      }
     }
-}
-
-const prevButton = document.getElementById('prevButton');
-const nextButton = document.getElementById('nextButton');
-
-prevButton.addEventListener('click', function() {
-    const newIndex = (currentImageIndex - 1 + totalImages) % totalImages;
-    showImage(newIndex);
-});
-
-nextButton.addEventListener('click', function() {
-    const newIndex = (currentImageIndex + 1) % totalImages;
-    showImage(newIndex);
-});
-
-
-showImage(0);
-
-images.forEach((image, index) => {
-    const imageInfo = image.querySelector('.image-info');
-    image.addEventListener('mouseover', () => {
-        imageInfo.style.display = 'block';
+  
+    const prevButton = document.getElementById("prevButton");
+    const nextButton = document.getElementById("nextButton");
+  
+    prevButton.addEventListener("click", function () {
+      const newIndex = (currentImageIndex - 1 + totalImages) % totalImages;
+      showImage(newIndex);
     });
-    image.addEventListener('mouseout', () => {
-        imageInfo.style.display = 'none';
+  
+    nextButton.addEventListener("click", function () {
+      const newIndex = (currentImageIndex + 1) % totalImages;
+      showImage(newIndex);
     });
-});
+  
+    showImage(0);
+  
+    images.forEach((image, index) => {
+      const imageInfo = image.querySelector(".image-info");
+      image.addEventListener("mouseover", () => {
+        imageInfo.style.display = "block";
+      });
+      image.addEventListener("mouseout", () => {
+        imageInfo.style.display = "none";
+      });
+    });
+  });
+  
